03c6a9bf7e2c676359dfe6de4fd099dfbdb64fb2
Nasser Hussein <naser738nas@gmail.com>
2022-10-17 13:22:50 +0300

    Services & Destination

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -1,13 +1,14 @@
 import Hero from "../../components/Hero/Hero";
 import Nav from "../../components/Nav/Nav";
-import Services from "../../Services/Services";
-
+import Services from "../../components/Services/Services";
+import TopDestinations from "../../components/TopDestinations/TopDestinations";
 const Home = () => {
     return(
         <>
             <Nav/>
             <Hero />
             <Services />
+            <TopDestinations />
         </>
     )
 }
